8b2faf8b933c2e22e82e2b68e7a445c24e917593
Gabriel Dittrich <gdittrchcj@gmail.com>
2024-03-26 09:06:26 -0300

    Aula 04

---
+++ ExClasses/Calculadora.cs
@@ -0,0 +1,21 @@
+class calculadora
+{
+    //Funções aritimeticas
+    public static double Somar(double a, double b)
+    {
+        return a + b;
+    }
+    public static double Subtrair(double a, double b)
+    {
+        return a - b;
+    }
+    public static double Multiplicar(double a, double b)
+    {
+        return a * b;
+    }
+    public static double Dividir(double a, double b)
+    {
+        return a / b;
+    }
+}
+
